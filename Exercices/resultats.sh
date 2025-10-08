b04c295465b67f1569c97cb156f6810e9b29f7f3
Nory09 <noraylanne.b2004@gmail.com>
2025-10-08 11:06:27 +0200

    Exercice TP1

---
+++ Exercices/resultats.sh
@@ -0,0 +1,13 @@
+ #!/usr/bin/bash
+ echo "Nombre d'annotations 2016 :"
+ cat ./Exercice1/ann/2016/2016* | wc -l
+ echo "Nombre d'annotations 2017 :"
+ cat ./Exercice1/ann/2017/2017* | wc -l
+ echo "Nombre d'annotations 2018 :"
+ cat ./Exercice1/ann/2018/2018* | wc -l
+ echo "Nombre d'occurrences de \"Location\" 2016 :"
+ cat ./Exercice1/ann/2016/2016* | grep Location | wc -l
+ echo "Nombre d'occurrences de \"Location\" 2017 :"
+ cat ./Exercice1/ann/2017/2017* | grep Location | wc -l
+ echo "Nombre d'occurrences de \"Location\" 2018 :"
+ cat ./Exercice1/ann/2018/2018* | grep Location | wc -l
